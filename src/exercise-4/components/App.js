23c331aabd4c8a3e7634f703b086b0dc2a4801e7
chenzdido <chenzdido@gmail.com>
2020-08-12 22:18:27 +0800

    [zhe] feat:complete exercise-4.

--- src/exercise-4/components/App.js
+++ src/exercise-4/components/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter as Router,  Switch} from 'react-router-dom';
 import {Route} from "react-router";
 import Home from "./Home";
 import About from "./About";
@@ -10,13 +10,15 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <Router>
-            <Route component={NotMatch} />
-            <Route path='/' component={Home} />
-            <Route path='/:user' component={User} />
+      <Router>
+      <Switch>
+            <Route exact path='/' component={Home} /> 
+            <Route path='/:user(\d+)' component={User} />
             <Route path='/about' component={About} />
-        </Router>
-      </div>
+            <Route component={NotMatch} />
+        </Switch>
+      </Router>
+    </div>
     );
   }
 }
